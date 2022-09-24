2142ec71d9ba33695e200d3fcefe6be2167f9df0
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-09-24 17:18:37 +0530

    modified:logic for email id

--- src/com/regex/UserRegistration.java
+++ src/com/regex/UserRegistration.java
@@ -11,11 +11,16 @@ public class UserRegistration {
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter your last name :");
             String lastName = sc.nextLine();
+            Scanner mail = new Scanner(System.in);
+            System.out.println("Enter your email : ");
+            String Email = mail.nextLine();
 
             System.out.println("first name is : " +firstName);
             System.out.println("first name is valid: " + firstName(firstName));
             System.out.println("last name is : " + lastName);
             System.out.println("last name is : " + lastName(lastName));
+            System.out.println("EmailID is : " + Email);
+            System.out.println("EmailID is : " + Email(Email));
         }
         private static boolean firstName(String firstName) {
             return firstName.matches("[A-Z]{1}[a-z]{3,}"); //UC1
@@ -23,5 +28,8 @@ public class UserRegistration {
     private static boolean lastName(String lastName) {
 
         return lastName.matches("[A-Z]{1}[a-z]{3}"); //UC2
+    }
+    public static boolean Email(String Email) {
+        return Email.matches("^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?"); //UC3
     }
 }
