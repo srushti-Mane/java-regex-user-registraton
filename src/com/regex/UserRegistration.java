5a8fdde320286f138e8e578a481e3dba13b94e53
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-09-24 17:10:52 +0530

    modified:logic for last name

--- src/com/regex/UserRegistration.java
+++ src/com/regex/UserRegistration.java
@@ -8,11 +8,20 @@ public class UserRegistration {
             Scanner scan = new Scanner(System.in);
             System.out.println("Enter your first name :");
             String firstName = scan.nextLine();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your last name :");
+            String lastName = sc.nextLine();
 
             System.out.println("first name is : " +firstName);
             System.out.println("first name is valid: " + firstName(firstName));
+            System.out.println("last name is : " + lastName);
+            System.out.println("last name is : " + lastName(lastName));
         }
         private static boolean firstName(String firstName) {
             return firstName.matches("[A-Z]{1}[a-z]{3,}"); //UC1
         }
+    private static boolean lastName(String lastName) {
+
+        return lastName.matches("[A-Z]{1}[a-z]{3}"); //UC2
+    }
 }
